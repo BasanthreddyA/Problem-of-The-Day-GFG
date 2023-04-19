22a672cf07ade11b9ac1e695f3ea27872f717172
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-04-19 22:26:19 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Four Elements/four-elements.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+import java.util.Arrays;
+import java.util.Scanner;
+class FindFourElements
+{
+	
+    public static void main(String[] args) 
+    {
+		Scanner sc=new Scanner (System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			FindFourElements findfour = new FindFourElements();
+			int n=sc.nextInt();
+			int[] A=new int[n];
+			for(int i=0;i<n;i++)
+				A[i]=sc.nextInt();
+			int X =sc.nextInt();
+			
+			Compute ob = new Compute();
+		    System.out.println(ob.find4Numbers(A, n, X)?1:0);
+		}
+    }
+}
+
+
+// } Driver Code Ends
+
+
+class Compute
+{
+    boolean find4Numbers(int a[], int n, int X) 
+    {
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                for(int k=j+1;k<n-1;k++){
+                    for(int l=k+1;l<n;l++){
+                        if(a[i]+a[j]+a[k]+a[l]==X){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+        
+    }
+}
