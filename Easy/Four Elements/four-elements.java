74167dc8b3393953ef78972b7a74032dd9478ba9
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-04-30 01:28:48 +0530

    Updated Solution - GfG to GitHub

--- Easy/Four Elements/four-elements.java
+++ Easy/Four Elements/four-elements.java
@@ -25,23 +25,34 @@ class FindFourElements
 
 // } Driver Code Ends
 
-
 class Compute
 {
-    boolean find4Numbers(int a[], int n, int X) 
+    boolean find4Numbers(int A[], int n, int X) 
     {
-        for(int i=0;i<n-3;i++){
-            for(int j=i+1;j<n-2;j++){
-                for(int k=j+1;k<n-1;k++){
-                    for(int l=k+1;l<n;l++){
-                        if(a[i]+a[j]+a[k]+a[l]==X){
-                            return true;
-                        }
-                    }
+        int l, r;
+        Arrays.sort(A);
+		boolean flag=false;
+        for (int i = 0; i < n - 3; i++) 
+        {
+            for (int j = i + 1; j < n - 2; j++) 
+            {
+                l = j + 1;
+                r = n - 1;
+                while (l < r) 
+                {
+                    if (A[i] + A[j] + A[l] + A[r] == X) 
+                    {
+						flag=true;
+                        l++;
+                        r--;
+                    } 
+                    else if (A[i] + A[j] + A[l] + A[r] < X)
+                        l++;
+                    else
+                        r--;
                 }
             }
         }
-        return false;
-        
-    }
+        return flag;
+	}
 }
