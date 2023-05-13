3bf501b31c7006c1ed2fe7390bcf5616e5e8891a
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-05-14 01:42:06 +0530

    Update max-sum-subarray-of-size-k.java

--- Basic/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
+++ Basic/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -1,3 +1,32 @@
+Apporach>>>
+Sure! Here's the approach of the `maximumSumSubarray` function in a simplified and easy-to-understand way:
+
+1. Initialize `ws` (window start) to 0, `sum` to 0, and `max` to the minimum value of `Long`.
+2. Iterate over the elements of the array from index 0 to `N-1`. The iterator variable is `we` (window end).
+3. Add the element at index `we` to the current sum.
+4. Update `max` to the larger value between `max` and the current sum.
+5. Check if the window size (`we - ws + 1`) is greater than or equal to `K` (the desired subarray length).
+   - If the window size is equal to `K`, update `max` to the larger value between `max` and the current sum.
+   - If the window size is greater than `K`, it means we need to slide the window.
+     - Subtract the element at index `ws` from the sum to remove it from the current window.
+     - Increment `ws` to slide the window to the right.
+6. Repeat steps 3 to 5 until we reach the end of the array.
+7. Return `max`, which represents the maximum sum of a subarray of length `K` within the given array.
+
+By following this approach, we maintain a sliding window of size `K` and keep track of the maximum sum encountered. The window slides through the array, updating the sum and maximum sum as necessary, to find the maximum sum subarray.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
 //{ Driver Code Starts
 //Initial Template for Java
 import java.io.*;
