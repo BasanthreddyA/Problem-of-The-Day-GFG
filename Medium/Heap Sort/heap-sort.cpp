3862daa0b8458a232e7c16369abf92aaca84f511
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-05-28 23:06:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Heap Sort/heap-sort.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// C++ program for implementation of Heap Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// The functions should be written in a way that array become sorted 
+// in increasing order when heapSort() is called.
+
+class Solution
+{
+    public:
+     void heapSort(int arr[], int n){
+    priority_queue<int, vector<int>, greater<int> > pq;
+    for(int i=0; i<n; i++){
+        pq.push(arr[i]);
+       }
+        int i = 0;
+        while(!pq.empty()){
+            arr[i] = pq.top();
+            pq.pop();
+            i++;
+        }
+    }
+
+
+};
+
+
+
+
+//{ Driver Code Starts.
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
