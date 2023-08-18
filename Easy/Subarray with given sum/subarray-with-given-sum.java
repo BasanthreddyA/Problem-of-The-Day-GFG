576e970c693249e92d010c918c4ba8be799f4765
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-08-19 01:10:52 +0530

    Updated Solution - GfG to GitHub

--- Easy/Subarray with given sum/subarray-with-given-sum.java
+++ Easy/Subarray with given sum/subarray-with-given-sum.java
@@ -35,39 +35,38 @@ class Main{
 }
 // } Driver Code Ends
 
+
 class Solution
 {
-    //Function to find a continuous sub-array which adds up to a given number.
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
     {
-        // Your code here
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        if(s==0){
-            a.add(-1);
-            return a;
-        }
-        int i=0,j=0;
-        int sum = arr[0];
-        while(j<n){
-            if(sum == s){
-                
-                a.add(i+1);
-                a.add(j+1);
-                return a;
+        ArrayList<Integer> res = new ArrayList<>();
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        
+        while (r < n) {
+            sum += arr[r];
+            if (sum < s) {
+                r++;
             }
-            else if(sum<s){
-                j++;
-                if(j<n){
-                    sum += arr[j];
+            else {
+                while (sum > s && l < r) {
+                    sum -= arr[l];
+                    l++;
                 }
-            }else{
-                sum -= arr[i];
-                i++;
+                if (sum == s) {
+                    res.add(l + 1);
+                    res.add(r + 1);
+                    break;
+                }
+                r++;
             }
         }
         
-        a.add(-1);
-        return a;
-        
+        if (res.isEmpty()) {
+            res.add(-1);
+        }
+        return res;
     }
 }
