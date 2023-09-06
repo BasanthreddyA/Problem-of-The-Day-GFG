fd270e8f7f50194b948c1f9018d8cb7342ddae6b
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-09-06 23:29:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Mother Vertex/mother-vertex.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+            }
+            Solution obj = new Solution();
+            int ans = obj.findMotherVertex(V, adj);
+            System.out.println(ans);
+       }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to find a Mother Vertex in the Graph.
+    static void Dfs(ArrayList<ArrayList<Integer>>adj, int curr, boolean vis[]){
+        vis[curr]=true;
+        for(int x:adj.get(curr)){
+            if(!vis[x]){
+                Dfs(adj,x,vis);
+            }
+        }
+    }
+
+
+    //Function to find a Mother Vertex in the Graph.
+    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+       boolean []vis=new boolean [V];
+       
+       int v=-1;
+       for(int i=0;i<V;i++){
+         if(!vis[i]){
+                Dfs(adj,i,vis);
+                v=i;
+            }
+       }
+       
+       
+        boolean []check=new boolean [V];
+        Dfs(adj,v,check);
+    
+    for(boolean val:check){
+        if(!val){
+            return -1;
+        }
+    }
+     return v;
+    
+  }
+}
