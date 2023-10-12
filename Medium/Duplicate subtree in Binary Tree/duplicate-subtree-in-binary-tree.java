8a76444e137c419f36f8fb8851e229589e50cc33
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-10-12 12:09:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Duplicate subtree in Binary Tree/duplicate-subtree-in-binary-tree.java
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	
+            Solution ob = new Solution();
+    		System.out.println(ob.dupSub(root));
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node
+
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+class Solution {
+    String helper(Node node, HashSet<String> set, boolean[] f1) {
+        if (node == null) {
+            return "$";
+        }
+        String str = "";
+        str = helper(node.left, set, f1) + "-";
+        str += node.data + "-";
+        str += helper(node.right, set, f1);
+        if (!set.contains(str)) {
+            set.add(str);
+        } else {
+            if (str.length() >= 11) {
+                f1[0] = true;
+            }
+        }
+        return str;
+    }
+
+    int dupSub(Node root) {
+        // code here
+        String s = "";
+        HashSet<String> set = new HashSet<>();
+        boolean[] f1 = new boolean[1];
+        helper(root, set, f1);
+        return f1[0] ? 1 : 0;
+    }
+}
