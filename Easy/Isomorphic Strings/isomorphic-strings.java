2951a8329420f79678f3b3dcef25d3f6552be955
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-11-11 22:28:30 +0530

    Updated Solution - GfG to GitHub

--- Easy/Isomorphic Strings/isomorphic-strings.java
+++ Easy/Isomorphic Strings/isomorphic-strings.java
@@ -31,26 +31,33 @@ public class Driver {
 
 // } Driver Code Ends
 
+
 class Solution
 {
     //Function to check if two strings are isomorphic.
     public static boolean areIsomorphic(String str1,String str2)
     {
-        if(str1.length() != str2.length()) return false;
-        HashMap<Character, Character> charCount= new HashMap();
-        char c = 'a';
-        for (int i = 0; i < str1.length(); i++) {
-            if (charCount.containsKey(str1.charAt(i))) {
-                c = charCount.get(str1.charAt(i));
-                if (c != str2.charAt(i))
-                    return false;
-            }
-            else if (!charCount.containsValue(str2.charAt(i))) {
-                charCount.put(str1.charAt(i),str2.charAt(i));
-            }
-            else {
-                return false;
-            }
+        
+         if (str1.length()!=str2.length())
+        return false;
+        HashMap<Character,Character> hs1=new HashMap<>();
+        HashMap<Character,Character> hs2=new HashMap<>();
+        for(int i=0;i<str1.length();i++)
+        {
+          if (!hs1.containsKey(str1.charAt(i)))
+            hs1.put(str1.charAt(i),str2.charAt(i));
+            else 
+            if(hs1.get(str1.charAt(i))!=str2.charAt(i))
+            return false;
+        }
+       
+        for(int i=0;i<str1.length();i++)
+        {
+          if (!hs2.containsKey(str2.charAt(i)))
+            hs2.put(str2.charAt(i),str1.charAt(i));
+            else 
+            if(hs2.get(str2.charAt(i))!=str1.charAt(i))
+            return false;
         }
         return true;
     }
