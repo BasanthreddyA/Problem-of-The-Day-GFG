3d4e1d81a2c70b1d0109ea21540ed9eba288116c
A Basanth Reddy <99126115+BasanthreddyA@users.noreply.github.com>
2023-12-01 11:05:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Check whether BST contains Dead End/check-whether-bst-contains-dead-end.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+
+import java.util.*;
+import java.io.*;
+class Node {
+        int data;
+        Node left, right;
+ 
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+class BinarySearchTree {
+    public static Node root;
+    BinarySearchTree() { 
+        root = null; 
+    }
+    void insert(int key) {
+       root = insertRec(root, key);
+    }
+    Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.data)
+            root.left = insertRec(root.left, key);
+        else if (key > root.data)
+            root.right = insertRec(root.right, key);
+        return root;
+    }
+ 
+    public static void inorder()  {
+       inorderRec(root);
+    }
+    public static void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data);
+            inorderRec(root.right);
+        }
+    }
+ 
+    // Driver Program to test above functions
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int []a=new int[n];
+            BinarySearchTree obj1=new BinarySearchTree();
+            for(int i=0;i<n;i++)
+            {
+                int b=sc.nextInt();
+                obj1.insert(b);
+            }
+            //inorder();
+            Solution obj=new Solution();
+            boolean k=obj.isDeadEnd(root);
+            if(k==true)
+            System.out.println("1");
+            else
+            System.out.println("0");
+        }       
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*class Node {
+        int data;
+        Node left, right;
+ 
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }*/
+
+//Function should return true if a deadEnd is found in the bst otherwise return false.
+class Solution
+{
+    public static boolean isDeadEnd(Node root)
+    {
+        //Add your code here.
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(0);
+        helper(al, root);
+        return sol(al, root);
+    }
+    
+    private static void helper(ArrayList<Integer> al, Node root) {
+        if(root==null) return;
+        helper(al, root.left);
+        al.add(root.data);
+        helper(al, root.right);
+        return;
+    }
+    
+    private static boolean sol(ArrayList<Integer> al, Node root) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null ) 
+        if(al.contains(root.data-1) && al.contains(root.data+1))
+        return true;
+        return sol(al, root.left) || sol(al, root.right);
+    }
+ 
+}
